a1bd9534474c1082331d5d528fc9f5c7fa4053fd
Pocol <project.asura@gmail.com>
2015-04-09 10:55:14 +0900

    エラーが出ないように修正。

--- D3D12_Simple/include/App.h
+++ D3D12_Simple/include/App.h
@@ -40,7 +40,7 @@ public:
     //=============================================================================================
     // public variables.
     //=============================================================================================
-
+    /* NOTHING */
 
     //=============================================================================================
     // public methods.
@@ -49,58 +49,58 @@ public:
     virtual ~App();
     void Run();
 
-
 protected:
     //=============================================================================================
     // protected variables.
     //=============================================================================================
-    HINSTANCE           m_hInst;
-    HWND                m_hWnd;
-    D3D_DRIVER_TYPE     m_DriverType;
-    D3D_FEATURE_LEVEL   m_FeatureLevel;
-    DXGI_FORMAT         m_SwapChainFormat;
-    DXGI_FORMAT         m_DepthStencilFormat;
-
-
+    HINSTANCE           m_hInst;            //!< インスタンスハンドルです.
+    HWND                m_hWnd;             //!< ウィンドウハンドルです.
+    UINT                m_BufferCount;      //!< バッファ数です.
+    DXGI_FORMAT         m_SwapChainFormat;  //!< スワップチェインのフォーマットです.
+    D3D12_VIEWPORT      m_Viewport;         //!< ビューポートです.
 
     //=============================================================================================
     // protected methods.
     //=============================================================================================
-    virtual bool OnInit();
-    virtual void OnTerm();
-    virtual void OnFrameMove();
-    virtual void OnFrameRender();
-    virtual void OnResize();
+    virtual bool OnInit         ();
+    virtual void OnTerm         ();
+    virtual void OnFrameMove    ();
+    virtual void OnFrameRender  ();
+    virtual void OnResize       ( u32 width, u32 height );
 
-    void SetResourceBarrier( ID3D12GraphicsCommandList* pCmdList, ID3D12Resource* pResource, UINT stateBefore, UINT stateAfter );
+    void SetResourceBarrier( 
+        ID3D12GraphicsCommandList*  pCmdList,
+        ID3D12Resource*             pResource,
+        UINT                        stateBefore,
+        UINT                        stateAfter );
+    void Present( u32 syncInterval );
 
 private:
     //=============================================================================================
     // private variables.
     //=============================================================================================
-    asdx::RefPtr<ID3D12Device>              m_Device;
-    asdx::RefPtr<ID3D12CommandAllocator>    m_CmdAllocator;
-    asdx::RefPtr<ID3D12CommandQueue>        m_CmdQueue;
-    asdx::RefPtr<ID3D12GraphicsCommandList> m_GfxCmdList;
-    asdx::RefPtr<IDXGIFactory>              m_Factory;
-    asdx::RefPtr<IDXGISwapChain>            m_SwapChain;
-    asdx::RefPtr<ID3D12DescriptorHeap>      m_DescriptorHeap;
-    asdx::RefPtr<ID3D12RootSignature>       m_RootSignature;
-    asdx::RefPtr<ID3D12Resource>            m_ColorTarget;
-    asdx::RefPtr<ID3D12Resource>            m_DepthStencilTarget;
-    asdx::RefPtr<ID3D12Fence>               m_Fence;
-    D3D12_VIEWPORT                          m_Viewport;
-    D3D12_CPU_DESCRIPTOR_HANDLE             m_ColorTargetHandle;
+    asdx::RefPtr<ID3D12Device>              m_Device;                   //!< デバイスです.
+    asdx::RefPtr<ID3D12CommandAllocator>    m_CmdAllocator;             //!< コマンドアロケータです.
+    asdx::RefPtr<ID3D12CommandQueue>        m_CmdQueue;                 //!< コマンドキューです.
+    asdx::RefPtr<ID3D12GraphicsCommandList> m_CmdList;                  //!< コマンドリストです.
+    asdx::RefPtr<IDXGIFactory>              m_Factory;                  //!< DXGIファクトリーです.
+    asdx::RefPtr<IDXGISwapChain>            m_SwapChain;                //!< スワップチェインです.
+    asdx::RefPtr<ID3D12DescriptorHeap>      m_DescriptorHeap;           //!< デスクリプターヒープです.
+    asdx::RefPtr<ID3D12RootSignature>       m_RootSignature;            //!< ルートシグネチャです.
+    asdx::RefPtr<ID3D12Resource>            m_ColorTarget;              //!< カラーターゲットのリソースです.
+    asdx::RefPtr<ID3D12Fence>               m_Fence;                    //!< フェンスです.
+    D3D12_CPU_DESCRIPTOR_HANDLE             m_ColorTargetHandle;        //!< カラーターゲットのハンドルです.
+    HANDLE                                  m_EventHandle;              //!< イベントハンドルです.
 
     //=============================================================================================
     // private methods.
     //=============================================================================================
-    bool InitApp();
-    void TermApp();
-    bool InitWnd();
-    void TermWnd();
-    bool InitD3D();
-    void TermD3D();
+    bool InitApp ();
+    void TermApp ();
+    bool InitWnd ();
+    void TermWnd ();
+    bool InitD3D ();
+    void TermD3D ();
     void MainLoop();
 
     static LRESULT CALLBACK MsgProc(HWND hWnd, UINT uMsg, WPARAM wp, LPARAM lp);
--- D3D12_Simple/src/App.cpp
+++ D3D12_Simple/src/App.cpp
@@ -11,6 +11,7 @@
 #include <cstdio>
 #include <array>
 
+
 #ifndef DLOG
 #if defined(DEBUG) || defined(_DEBUG)
 #define DLOG(x, ...)       printf_s("[File: %s, Line:%d] "x"\n", __FILE__, __LINE__, ##__VA_ARGS__)
@@ -28,20 +29,44 @@
 #endif//ASDX_WND_CLASSNAME
 
 
+namespace /* anonymous */ {
+
+//-------------------------------------------------------------------------------------------------
+// Global Variables.
+//-------------------------------------------------------------------------------------------------
+App*    g_pApp = nullptr;
+
+
+} // namespace /* anonymous */
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // App class
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+//-------------------------------------------------------------------------------------------------
+//      コンストラクタです.
+//-------------------------------------------------------------------------------------------------
 App::App()
-{
-}
+: m_hInst           ( nullptr )
+, m_hWnd            ( nullptr )
+, m_BufferCount     ( 2 )
+, m_SwapChainFormat ( DXGI_FORMAT_R8G8B8A8_UNORM_SRGB )
+, m_EventHandle     ( nullptr )
+{ /* DO_NOTHING */ }
 
+//-------------------------------------------------------------------------------------------------
+//      デストラクタです.
+//-------------------------------------------------------------------------------------------------
 App::~App()
-{
-}
+{ TermApp(); }
 
+//-------------------------------------------------------------------------------------------------
+//      初期化処理です.
+//-------------------------------------------------------------------------------------------------
 bool App::InitApp()
 {
+    // COMライブラリの初期化.
     HRESULT hr = CoInitialize( nullptr );
     if ( FAILED( hr ) )
     {
@@ -49,30 +74,40 @@ bool App::InitApp()
         return false;
     }
 
+    // ウィンドウの初期化.
     if ( !InitWnd() )
     {
         ELOG( "Error : InitWnd() Failed." );
         return false;
     }
 
+    // D3D12の初期化.
     if ( !InitD3D() )
     {
         ELOG( "Error : InitD3D() Failed." );
         return false;
     }
 
+    // アプリケーション固有の初期化.
     if ( !OnInit() )
     {
         ELOG( "Error : OnInit() Failed." );
         return false;
     }
 
+    // ポインタ設定.
+    g_pApp = this;
+
+    // 正常終了.
     return true;
 }
 
-
+//-------------------------------------------------------------------------------------------------
+//      ウィンドウの初期化処理です.
+//-------------------------------------------------------------------------------------------------
 bool App::InitWnd()
 {
+    // インスタンスハンドルを取得.
     HINSTANCE hInst = GetModuleHandle( nullptr );
     if ( !hInst )
     {
@@ -80,33 +115,38 @@ bool App::InitWnd()
         return false;
     }
 
+    // 拡張ウィンドウクラスの設定.
     WNDCLASSEXW wc;
-    wc.cbSize = sizeof(WNDCLASSEXW);
-    wc.style = CS_HREDRAW | CS_VREDRAW;
-    wc.lpfnWndProc = MsgProc;
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hInstance = hInst;
-    wc.hIcon = LoadIcon( hInst, IDI_APPLICATION );
-    wc.hCursor = LoadCursor( nullptr, IDC_ARROW );
-    wc.hbrBackground = (HBRUSH)( COLOR_WINDOW + 1 );
-    wc.lpszMenuName = nullptr;
-    wc.lpszClassName = ASDX_WND_CLASSNAME;
-    wc.hIconSm = LoadIcon( hInst, IDI_APPLICATION );
+    wc.cbSize           = sizeof(WNDCLASSEXW);
+    wc.style            = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc      = MsgProc;
+    wc.cbClsExtra       = 0;
+    wc.cbWndExtra       = 0;
+    wc.hInstance        = hInst;
+    wc.hIcon            = LoadIcon( hInst, IDI_APPLICATION );
+    wc.hCursor          = LoadCursor( nullptr, IDC_ARROW );
+    wc.hbrBackground    = (HBRUSH)( COLOR_WINDOW + 1 );
+    wc.lpszMenuName     = nullptr;
+    wc.lpszClassName    = ASDX_WND_CLASSNAME;
+    wc.hIconSm          = LoadIcon( hInst, IDI_APPLICATION );
 
+    // 拡張ウィンドウクラスを登録.
     if ( !RegisterClassExW( &wc ) )
     {
         ELOG( "Error : RegisterClassEx() Failed." );
         return false;
     }
 
+    // インスタンスハンドルを設定.
     m_hInst = hInst;
 
     RECT rc = { 0, 0, 960, 540 };
 
+    // ウィンドウの矩形を調整.
     DWORD style = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MAXIMIZEBOX | WS_MINIMIZEBOX;
     AdjustWindowRect( &rc, style, FALSE );
 
+    // ウィンドウを生成.
     m_hWnd = CreateWindowW(
         ASDX_WND_CLASSNAME,
         TEXT("SimpleSample"),
@@ -120,33 +160,44 @@ bool App::InitWnd()
         m_hInst,
         nullptr );
 
+    // エラーチェック.
     if ( !m_hWnd )
     {
         ELOG( "Error : CreateWindowW() Failed." );
         return false;
     }
 
+    // ウィンドウを表示
     ShowWindow( m_hWnd, SW_SHOWNORMAL );
     UpdateWindow( m_hWnd );
+
+    // フォーカス設定.
     SetFocus( m_hWnd );
 
+    // 正常終了.
     return true;
 }
 
+//-------------------------------------------------------------------------------------------------
+//      D3D12の初期化処理です.
+//-------------------------------------------------------------------------------------------------
 bool App::InitD3D()
 {
     HRESULT hr = S_OK;
 
+    // ウィンドウ幅を取得.
     RECT rc;
     GetClientRect( m_hWnd, &rc );
     u32 w = rc.right - rc.left;
     u32 h = rc.bottom - rc.top;
 
+    // デバイス生成フラグ.
     auto createDeviceFlags = D3D12_CREATE_DEVICE_NONE;
 #if defined(DEBUG) || defined(_DEBUG)
     createDeviceFlags |= D3D12_CREATE_DEVICE_DEBUG;
 #endif
 
+    // ドライバータイプ.
     std::array<D3D_DRIVER_TYPE, 4> driverTypes = { 
         D3D_DRIVER_TYPE_HARDWARE,
         D3D_DRIVER_TYPE_WARP,
@@ -154,6 +205,7 @@ bool App::InitD3D()
         D3D_DRIVER_TYPE_SOFTWARE
     };
 
+    // デバイス生成.
     for( size_t i=0; i<driverTypes.size(); ++i )
     {
         hr = D3D12CreateDevice(
@@ -166,17 +218,17 @@ bool App::InitD3D()
             (void**)m_Device.GetAddress() );
 
         if ( SUCCEEDED( hr ) )
-        {
-            break;
-        }
+        { break; }
     }
 
+    // 生成チェック.
     if ( FAILED( hr ) )
     {
         ELOG( "Error : D3D12CreateDevice()Failed." );
         return false;
     }
 
+    // コマンドアロケータを生成.
     hr = m_Device->CreateCommandAllocator(
         D3D12_COMMAND_LIST_TYPE_DIRECT, 
         IID_ID3D12CommandAllocator,
@@ -187,6 +239,7 @@ bool App::InitD3D()
         return false;
     }
 
+    // コマンドキューを生成.
     {
        D3D12_COMMAND_QUEUE_DESC desc;
        ZeroMemory( &desc, sizeof(desc) );
@@ -202,6 +255,7 @@ bool App::InitD3D()
        }
     }
 
+    // スワップチェインを生成.
     {
         hr = CreateDXGIFactory( IID_IDXGIFactory, (void**)m_Factory.GetAddress() );
         if ( FAILED( hr ) )
@@ -212,8 +266,8 @@ bool App::InitD3D()
 
         DXGI_SWAP_CHAIN_DESC desc;
         ZeroMemory( &desc, sizeof(desc) );
-        desc.BufferCount                        = 2;
-        desc.BufferDesc.Format                  = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+        desc.BufferCount                        = m_BufferCount;
+        desc.BufferDesc.Format                  = m_SwapChainFormat;
         desc.BufferDesc.Width                   = w;
         desc.BufferDesc.Height                  = h;
         desc.BufferDesc.RefreshRate.Numerator   = 60;
@@ -224,6 +278,7 @@ bool App::InitD3D()
         desc.SampleDesc.Quality                 = 0;
         desc.Windowed                           = TRUE;
 
+        // アダプター単位の処理にマッチするのは m_Device ではなく m_CmdQueue　なので，m_CmdQueue　を第一引数として渡す.
         hr = m_Factory->CreateSwapChain( m_CmdQueue.GetPtr(), &desc, m_SwapChain.GetAddress() );
         if ( FAILED( hr ) )
         {
@@ -232,11 +287,12 @@ bool App::InitD3D()
         }
     }
 
+    // デスクリプタヒープの生成.
     {
         D3D12_DESCRIPTOR_HEAP_DESC desc;
         ZeroMemory( &desc, sizeof(desc) );
 
-        desc.NumDescriptors = 2;
+        desc.NumDescriptors = 1;
         desc.Type           = D3D12_RTV_DESCRIPTOR_HEAP;
         desc.Flags          = D3D12_DESCRIPTOR_HEAP_NONE;
 
@@ -248,6 +304,7 @@ bool App::InitD3D()
         }
     }
 
+    // ルートシグネチャの生成.
     {
         auto rootSig = D3D12_ROOT_SIGNATURE();
         rootSig.Flags = D3D12_ROOT_SIGNATURE_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
@@ -274,6 +331,7 @@ bool App::InitD3D()
         }
     }
 
+    // コマンドリストの生成.
     {
         hr = m_Device->CreateCommandList(
             1,
@@ -281,7 +339,7 @@ bool App::InitD3D()
             m_CmdAllocator.GetPtr(),
             nullptr,
             IID_ID3D12GraphicsCommandList,
-            (void**)m_GfxCmdList.GetAddress() );
+            (void**)m_CmdList.GetAddress() );
         if ( FAILED( hr ) )
         {
             ELOG( "Error : ID3D12Device::CreateCommandList() Failed." );
@@ -289,6 +347,7 @@ bool App::InitD3D()
         }
     }
 
+    // バックバッファからレンダーターゲットを生成.
     {
         hr = m_SwapChain->GetBuffer( 0, IID_ID3D12Resource, (void**)m_ColorTarget.GetAddress() );
         if ( FAILED( hr ) )
@@ -301,8 +360,11 @@ bool App::InitD3D()
         m_Device->CreateRenderTargetView( m_ColorTarget.GetPtr(), nullptr, m_ColorTargetHandle );
     }
 
+    // フェンスの生成.
     {
-        hr = m_Device->CreateFence( 0, D3D12_FENCE_MISC_NONE, IID_ID3D12Fence, (void**)m_Factory.GetAddress() );
+        m_EventHandle = CreateEvent( 0, FALSE, FALSE, 0 );
+
+        hr = m_Device->CreateFence( 0, D3D12_FENCE_MISC_NONE, IID_ID3D12Fence, (void**)m_Fence.GetAddress() );
         if ( FAILED( hr ) )
         {
             ELOG( "Error : ID3D12Device::CreateFence() Failed." );
@@ -310,6 +372,7 @@ bool App::InitD3D()
         }
     }
 
+    // ビューポートの設定.
     {
         m_Viewport.TopLeftX = 0;
         m_Viewport.TopLeftY = 0;
@@ -319,22 +382,34 @@ bool App::InitD3D()
         m_Viewport.MaxDepth = 1.0f;
     }
 
-    m_GfxCmdList->Close();
-
+    // 正常終了.
     return true;
 }
 
+//-------------------------------------------------------------------------------------------------
+//      終了処理です.
+//-------------------------------------------------------------------------------------------------
 void App::TermApp()
 {
+    // アプリケーション固有の終了処理.
     OnTerm();
 
+    // D3D12の終了処理.
     TermD3D();
 
+    // ウィンドウの終了処理.
     TermWnd();
 
+    // COMライブラリの終了処理.
     CoUninitialize();
+
+    // ポインタクリア.
+    g_pApp = nullptr;
 }
 
+//-------------------------------------------------------------------------------------------------
+//      ウィンドウの終了処理.
+//-------------------------------------------------------------------------------------------------
 void App::TermWnd()
 {
     if ( m_hInst != nullptr )
@@ -344,10 +419,19 @@ void App::TermWnd()
     m_hWnd  = nullptr;
 }
 
+//-------------------------------------------------------------------------------------------------
+//      D3D12の終了処理.
+//-------------------------------------------------------------------------------------------------
 void App::TermD3D()
 {
+    CloseHandle( m_EventHandle );
+
+    m_EventHandle = nullptr;
 }
 
+//-------------------------------------------------------------------------------------------------
+//      メインループです.
+//-------------------------------------------------------------------------------------------------
 void App::MainLoop()
 {
     MSG msg = { 0 };
@@ -369,50 +453,89 @@ void App::MainLoop()
     }
 }
 
+//-------------------------------------------------------------------------------------------------
+//      アプリケーションを実行します.
+//-------------------------------------------------------------------------------------------------
 void App::Run()
 {
+    // 初期化に成功したらメインループに入る.
     if ( InitApp() )
-    {
-        MainLoop();
-    }
+    { MainLoop(); }
 
+    // 終了処理.
     TermApp();
 }
 
+//-------------------------------------------------------------------------------------------------
+//      アプリケーション固有の初期化処理です.
+//-------------------------------------------------------------------------------------------------
 bool App::OnInit()
 {
     return true;
 }
 
+//-------------------------------------------------------------------------------------------------
+//      アプリケーション固有の終了処理です.
+//-------------------------------------------------------------------------------------------------
 void App::OnTerm()
 {
 }
 
+//-------------------------------------------------------------------------------------------------
+//      フレーム遷移処理です.
+//-------------------------------------------------------------------------------------------------
 void App::OnFrameMove()
 {
 }
 
+//-------------------------------------------------------------------------------------------------
+//      フレーム描画処理です.
+//-------------------------------------------------------------------------------------------------
 void App::OnFrameRender()
 {
-    m_CmdAllocator->Reset();
-    m_GfxCmdList->Reset( m_CmdAllocator.GetPtr(), nullptr );
+    // ビューポートを設定.
+    m_CmdList->RSSetViewports( 1, &m_Viewport );
+    SetResourceBarrier( m_CmdList.GetPtr(), m_ColorTarget.GetPtr(), D3D12_RESOURCE_USAGE_PRESENT, D3D12_RESOURCE_USAGE_RENDER_TARGET );
 
-    m_GfxCmdList->RSSetViewports( 1, &m_Viewport );
-    SetResourceBarrier( m_GfxCmdList.GetPtr(), m_ColorTarget.GetPtr(), D3D12_RESOURCE_USAGE_PRESENT, D3D12_RESOURCE_USAGE_RENDER_TARGET );
-
+    // カラーバッファをクリア.
     float clearColor[] = { 0.39f, 0.58f, 0.92f, 1.0f };
-    m_GfxCmdList->ClearRenderTargetView( m_ColorTargetHandle, clearColor, nullptr, 0 );
-    SetResourceBarrier( m_GfxCmdList.GetPtr(), m_ColorTarget.GetPtr(), D3D12_RESOURCE_USAGE_RENDER_TARGET, D3D12_RESOURCE_USAGE_PRESENT );
+    m_CmdList->ClearRenderTargetView( m_ColorTargetHandle, clearColor, nullptr, 0 );
+    SetResourceBarrier( m_CmdList.GetPtr(), m_ColorTarget.GetPtr(), D3D12_RESOURCE_USAGE_RENDER_TARGET, D3D12_RESOURCE_USAGE_PRESENT );
 
-    m_GfxCmdList->Close();
-    m_CmdQueue->ExecuteCommandLists( 1, CommandListCast( m_GfxCmdList.GetAddress() ) );
-    m_SwapChain->Present( 1, 0 );
+    // 画面に表示.
+    Present( 0 );
 }
 
-void App::OnResize()
+//-------------------------------------------------------------------------------------------------
+//      リサイズ時の処理です.
+//-------------------------------------------------------------------------------------------------
+void App::OnResize( u32 width, u32 height )
 {
+    m_Viewport.Width  = FLOAT( width );
+    m_Viewport.Height = FLOAT( height );
+
+    // レンダーターゲットを破棄.
+    m_ColorTarget.Reset();
+    m_ColorTargetHandle.ptr = 0;
+
+    // バックバッファをリサイズ.
+    HRESULT hr = m_SwapChain->ResizeBuffers( m_BufferCount, 0, 0, m_SwapChainFormat, 0 );
+    if ( FAILED( hr ) )
+    { ELOG( "Error : IDXGISwapChain::ResizeBuffer() Failed." ); }
+
+    // バックバッファを取得.
+    hr = m_SwapChain->GetBuffer( 0, IID_ID3D12Resource, (void**)m_ColorTarget.GetAddress() );
+    if ( FAILED( hr ) )
+    { ELOG( "Error : IDXGISwapChain::GetBuffer() Failed." ); }
+
+    // レンダーターゲットを生成.
+    m_ColorTargetHandle = m_DescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+    m_Device->CreateRenderTargetView( m_ColorTarget.GetPtr(), nullptr, m_ColorTargetHandle );
 }
 
+//-------------------------------------------------------------------------------------------------
+//      リソースバリアの設定.
+//-------------------------------------------------------------------------------------------------
 void App::SetResourceBarrier
 (
     ID3D12GraphicsCommandList* pCmdList,
@@ -431,7 +554,32 @@ void App::SetResourceBarrier
     pCmdList->ResourceBarrier( 1, &desc );
 }
 
+//-------------------------------------------------------------------------------------------------
+//      コマンドを実行して画面に表示します.
+//-------------------------------------------------------------------------------------------------
+void App::Present( u32 syncInterval )
+{
+    // コマンドリストへの記録を終了し，コマンド実行.
+    m_CmdList->Close();
+    m_CmdQueue->ExecuteCommandLists( 1, CommandListCast( m_CmdList.GetAddress() ) );
 
+    // コマンドの実行の終了を待機する
+    m_Fence->Signal( 0 );
+    m_Fence->SetEventOnCompletion( 1, m_EventHandle );
+    m_CmdQueue->Signal( m_Fence.GetPtr(), 1 );
+    WaitForSingleObject( m_EventHandle, INFINITE );
+
+    // コマンドリストとコマンドアロケータをリセットする.
+    m_CmdAllocator->Reset();
+    m_CmdList->Reset( m_CmdAllocator.GetPtr(), nullptr );
+
+    // 画面に表示する.
+    m_SwapChain->Present( syncInterval, 0 );
+}
+
+//-------------------------------------------------------------------------------------------------
+//      ウィンドウプロシージャです.
+//-------------------------------------------------------------------------------------------------
 LRESULT CALLBACK App::MsgProc( HWND hWnd, UINT uMsg, WPARAM wp, LPARAM lp )
 {
     switch( uMsg )
@@ -449,6 +597,16 @@ LRESULT CALLBACK App::MsgProc( HWND hWnd, UINT uMsg, WPARAM wp, LPARAM lp )
                 PostQuitMessage( 0 );
             }
             break;
+
+        case WM_SIZE:
+            {
+                if ( g_pApp != nullptr )
+                {
+                    u32 w = LOWORD( lp );
+                    u32 h = HIWORD( lp );
+                    g_pApp->OnResize( w, h );
+                }
+            }
     }
 
     return DefWindowProc( hWnd, uMsg, wp, lp );
